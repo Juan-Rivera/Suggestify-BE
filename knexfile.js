335f7f35fa3d0f17f136dbeb6541d41d8e0f7c55
Juan Rivera <juanrivera.primary@gmail.com>
2021-03-05 12:31:54 -0500

    testing

--- knexfile.js
+++ knexfile.js
@@ -32,10 +32,9 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: false,
     },
     migrations: {
       directory: './data/migrations'
